cf001ff855505696cd8e939383b79b7891b5a26e
Dominic DeWolfe <domdewolfe@gmail.com>
2017-06-17 12:27:44 -0400

    rescue from http"

--- lib/cyptoify/technical/data.rb
+++ lib/cyptoify/technical/data.rb
@@ -4,7 +4,7 @@ module Cyptoify
       include Enumerable
       require 'rest-client'
 
-      attr_accessor :price_set,:time_stamp
+      attr_accessor :price_set, :time_stamp
 
       def initialize(**args)
         @file_name = args[:file] || 'price.json'
@@ -12,7 +12,7 @@ module Cyptoify
         @time_stamp = DateTime.now
       end
 
-      def each(&block)
+      def each()
         return enum_for(:each) unless block_given?
         send("each_#{@grain}") { |p| yield(p) }
       end
@@ -28,8 +28,14 @@ module Cyptoify
 
       def load_data_from_etherchain
         file = File.open('price.json', 'w')
+        begin
         request = RestClient.get('https://etherchain.org/api/statistics/price')
         file.write(request.body)
+
+      rescue Exception => e
+        puts "Error while loading data from EtherChain \n #{e}"
+      end
+
       end
 
       def each_daily
@@ -41,13 +47,12 @@ module Cyptoify
           if current_date == this_date_time.to_date
             current_price = price['usd'] if current_date_time <= this_date_time
           else
-            yield ({ date: current_date, usd: current_price })
+            yield { date: current_date, usd: current_price }
             current_date = this_date_time.to_date
           end
         end
-          yield ({ date: current_date, usd: current_price })
+        yield { date: current_date, usd: current_price }
       end
-
     end
   end
 end
